b25e1483dd767177f9e616539bc412402753f545
Muhammad Wijdan Haider <ghostwijdan12@gmail.com>
2024-06-05 11:39:10 +0500

    New IDE, Let's see if it works.

--- prototype/Modules/contemp.py
+++ prototype/Modules/contemp.py
@@ -1,7 +1,7 @@
 from tempfile import NamedTemporaryFile
 from mtcnn import MTCNN
 from PIL import Image
-
+import torchvision.transforms as transforms
 
 MINIMUM_CONFIDENCE = 0.95
 
@@ -15,22 +15,33 @@ async def retTempFile(file_content, suffixg):
     return temp_file_path
 
 
-def detect_faces(frame):
+async def detect_faces(frame):
     faces = detector.detect_faces(frame)
     return faces
 
 
-def cropping(data):
+async def cropping(data, input_image):
     face_data = data
-    # face_data = m_face_d["face_data"]
-    if not (face_data["confidence"] < MINIMUM_CONFIDENCE):
-        x, y, w, h = face_data[0]["box"]
-        x, y, w, h = max(x, 0), max(y, 0), max(w, 0), max(h, 0)
-        cropped_face = face_data["frame"][y : y + h, x : x + w]
-        cropped_face_shape = cropped_face.shape
-        cropped_face_size = (cropped_face_shape[1], cropped_face_shape[0])
-        new_image = Image.new("RGB", cropped_face_size, (0, 0, 0, 0))
-        new_image.paste(Image.fromarray(cropped_face), (0, 0))
-    else:
-        new_image = None
-    return new_image
+    t_faces = []
+    for face in face_data:
+        if not (face["confidence"] <= MINIMUM_CONFIDENCE):
+            x, y, w, h = face["box"]
+            x, y, w, h = max(x, 0), max(y, 0), max(w, 0), max(h, 0)
+            extracted_face = input_image.crop((x, y, x + w, y + h))
+            new_image = Image.new("RGB", extracted_face.size, (0, 0, 0, 0))
+            new_image.paste(extracted_face, (0, 0))
+            t_faces.append(new_image)
+        else:
+            new_image = None
+    return t_faces
+
+
+async def preprocessing_f(file_content):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((224, 224)),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    img = transform(file_content)
+    img = img.unsqueeze(0)
+    return img
--- prototype/Modules/imageprocessing.py
+++ prototype/Modules/imageprocessing.py
@@ -3,12 +3,38 @@ import cv2
 import base64
 from io import BytesIO
 from PIL import Image
+from Modules import contemp
+
+# for test purposes, will remove if errors occured and will go back to the normal flow of prcessing
+from fastapi import HTTPException
 
 
-async def preprocessing(file_content):
+async def face_detection_and_cropping(file_content):
     image_bytes = BytesIO(file_content)
     input_image = Image.open(image_bytes).convert("RGB")
+    # this conversion is for MTCNN
     pixels = np.array(input_image)
     pixels = pixels.astype("uint8")
+    faces = await contemp.detect_faces(pixels)
+    if len(faces) == 0:
+        raise HTTPException(status_code=400, detail="No faces detected in the image.")
+        # return -1
+    else:
+        faces = await contemp.cropping(faces, input_image)
+    return faces
 
-    return pixels
+
+async def complete_preprocessing(file_content):
+    image = face_detection_and_cropping(file_content)
+    if type(image) == dict and image == -1:
+        return -1
+    else:
+        if not len(image) == 0:
+
+            pass
+        else:
+            raise HTTPException(
+                status_code=400, detail="No faces detected in the image."
+            )
+        return image
+
